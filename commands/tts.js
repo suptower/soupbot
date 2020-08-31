611e44c91570abb96d16a0eb93ce7f6495e94340
suptower <70473844+suptower@users.noreply.github.com>
2020-08-31 18:41:53 +0200

    spiced up tts.js

--- commands/tts.js
+++ commands/tts.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'uses text-to-speech provided by google-api',
 	execute(message, args) {
         console.log("tts command has been initiated.");
-        if (message.member.voice.channel) {
+        if (!args.length && message.member.voice.channel) {
             const voiceChannel = message.member.voice.channel;
             const str1='http://api.voicerss.org/?key=de876d46f8d44255aa7b445628cc32a0&hl=de-de&v=Jonas&c=MP3&f=16khz_16bit_stereo&src=';
             const str2 = args.join(' ');
@@ -11,7 +11,27 @@ module.exports = {
             voiceChannel.join().then(connection => {
                 const dispatcher = connection.play(link);
                 dispatcher.on('finish',() => voiceChannel.leave(link));
-            })
+        });
+        
+    }
+    else if (!args.length) {
+        return message.reply('you need to be connected to a voice channel to use this command.');
+    }
+    else if (message.mentions.users.size>=1 && message.mentions.members.first().voice.channel) {
+        if (!message.member.permissions.has('MOVE_MEMBERS')) {
+            return message.reply("you are not allowed to use this command.\nYou need to be allowed to move members to execute this.");
         }
-	}
+        const voiceChannel = message.mentions.members.first().voice.channel;
+        const str1='http://api.voicerss.org/?key=de876d46f8d44255aa7b445628cc32a0&hl=de-de&v=Jonas&c=MP3&f=16khz_16bit_stereo&src=';
+        const str2 = args.join(' ');
+        const link = str1.concat(str2);
+        voiceChannel.join().then(connection => {
+            const dispatcher = connection.play(link);
+            dispatcher.on('finish',() => voiceChannel.leave(link));
+        })
+    }
+    else {
+        message.reply(`this user is currently not connected to any voice channel.`);
+    }
+},
 };
