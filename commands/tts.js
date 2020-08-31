7117b5308d816a742b464a074c5f2716d8548150
suptower <70473844+suptower@users.noreply.github.com>
2020-08-31 11:49:43 +0200

    Update tts.js

--- commands/tts.js
+++ commands/tts.js
@@ -6,24 +6,27 @@ module.exports = {
 	description: 'uses text-to-speech provided by google-api',
 	execute(message, args) {
         console.log("tts command has been initiated.");
-        const text = args.join(' ');
-        const request = {
-            input: {text: text},
-            voice: {languageCode: 'de-DE', ssmlGender: 'NEUTRAL'},
-            audioConfig: {audioEncoding: 'MP3'},
-        };
-        const [response] = await ttsClient.synthesizeSpeech(request);
-        const writeFile = util.promisify(fs.writeFile);
-        await writeFile('output.mp3', response.audioContent, 'binary');
-        console.log('Audio content written to output.mp3');
-        if (message.member.voice.channel) {
-            const voiceChannel = message.member.voice.channel;
-            voiceChannel.join().then(connection => {
-                const dispatcher = connection.play('output.mp3');
-                dispatcher.on("finish", () => {
-                    voiceChannel.leave();
+        async function startTTS() {
+            const text = args.join(' ');
+            const request = {
+                input: {text: text},
+                voice: {languageCode: 'de-DE', ssmlGender: 'NEUTRAL'},
+                audioConfig: {audioEncoding: 'MP3'},
+            };
+            const [response] = await ttsClient.synthesizeSpeech(request);
+            const writeFile = util.promisify(fs.writeFile);
+            await writeFile('output.mp3', response.audioContent, 'binary');
+            console.log('Audio content written to output.mp3');
+            if (message.member.voice.channel) {
+                const voiceChannel = message.member.voice.channel;
+                voiceChannel.join().then(connection => {
+                    const dispatcher = connection.play('output.mp3');
+                    dispatcher.on("finish", () => {
+                        voiceChannel.leave();
+                    })
                 })
-            })
+            }
         }
+
 	}
 };
