5487567690144fa95737c6c10f6560c99772dd00
suptower <70473844+suptower@users.noreply.github.com>
2020-08-31 14:10:18 +0200

    tts update

--- commands/tts.js
+++ commands/tts.js
@@ -1,33 +1,18 @@
 const Discord = require('discord.js');
-const textToSpeech = require('@google-cloud/text-to-speech');
-const ttsClient = new textToSpeech.TextToSpeechClient();
+const text2wav = require('text2wav');
+const assert = require('assert');
 module.exports = {
 	name: 'tts',
 	description: 'uses text-to-speech provided by google-api',
 	execute(message, args) {
         console.log("tts command has been initiated.");
-        async function startTTS() {
-            const text = args.join(' ');
-            const request = {
-                input: {text: text},
-                voice: {languageCode: 'de-DE', ssmlGender: 'NEUTRAL'},
-                audioConfig: {audioEncoding: 'MP3'},
-            };
-            const [response] = await ttsClient.synthesizeSpeech(request);
-            const writeFile = util.promisify(fs.writeFile);
-            await writeFile('output.mp3', response.audioContent, 'binary');
-            console.log('Audio content written to output.mp3');
-            if (message.member.voice.channel) {
-                const voiceChannel = message.member.voice.channel;
-                voiceChannel.join().then(connection => {
-                    const dispatcher = connection.play('output.mp3');
-                    dispatcher.on("finish", () => {
-                        voiceChannel.leave();
-                    })
-                })
-            }
+        let out = text2wav(args.join(' '));
+        if (message.member.voice.channel) {
+            const voiceChannel = message.member.voice.channel;
+            voiceChannel.join().then(connection => {
+                const dispatcher = connection.play(out);
+                dispatcher.on('finish',() => voiceChannel.leave());
+            })
         }
-        startTTS();
-
 	}
 };
--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
-const textToSpeech = require('@google-cloud/text-to-speech');
-const ttsClient = new textToSpeech.TextToSpeechClient();
+const text2wav = require('text2wav');
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const ytdl = require('ytdl-core');
