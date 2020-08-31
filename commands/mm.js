bdfbd6e4124333b7bfadf9de18a741a85395df37
suptower <70473844+suptower@users.noreply.github.com>
2020-08-31 21:52:45 +0200

    mm.js
    moooin meister

---
+++ commands/mm.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+module.exports = {
+	name: 'mm',
+	description: 'moooin meisteeer',
+	execute(message, args) {
+        console.log("mm command has been initiated.");
+        const value = Math.round(Math.random()*4);
+        switch (value) {
+            case 0:
+                const stream = 'https://media1.vocaroo.com/mp3/1d2uJN1BHCMS';
+                break;
+            case 1:
+                const stream = 'https://media1.vocaroo.com/mp3/1kKocCqcEZdV';
+                break;
+            case 2:
+                const stream = 'https://media1.vocaroo.com/mp3/1nv4IckXsCTl';
+                break;
+            case 3:
+                const stream = 'https://media1.vocaroo.com/mp3/1lwc7EQZYH4H';
+                break;
+            case 4:
+                const stream = 'https://media1.vocaroo.com/mp3/1axh0IStlpQ6';
+                break;
+            default:
+                const stream = 'https://media1.vocaroo.com/mp3/1axh0IStlpQ6';
+                break;
+        }
+
+    if (!args.length && message.member.voice.channel) {
+        const voiceChannel = message.member.voice.channel;
+
+        voiceChannel.join().then(connection => {
+            const dispatcher = connection.play(stream);
+            dispatcher.on('finish',() => voiceChannel.leave());
+    });
+        
+    }
+    else if (!args.length) {
+        return message.reply('you need to be connected to a voice channel to use this command.');
+    }
+    else if (message.mentions.users.size>=1 && message.mentions.members.first().voice.channel) {
+        if (!message.member.permissions.has('MOVE_MEMBERS')) {
+            return message.reply("you are not allowed to use this command.\nYou need to be allowed to move members to execute this.");
+        }
+        const voiceChannel = message.mentions.members.first().voice.channel;
+        voiceChannel.join().then(connection => {
+        const dispatcher = connection.play(stream);
+        dispatcher.on('finish',() => voiceChannel.leave());
+        })
+    
+    }
+    else {
+        message.reply(`this user is currently not connected to any voice channel.`);
+        }
+    },
+};
