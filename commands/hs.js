addcf9a080e2e219038afb796e2007d13eb5e863
suptower <70473844+suptower@users.noreply.github.com>
2020-08-31 08:56:40 +0200

    added sinne.js
    SINNEEE

--- commands/hs.js
+++ commands/hs.js
@@ -5,9 +5,6 @@ module.exports = {
 	description: 'Use the hs feature.',
 	execute(message, args) {
         console.log("hs command has been initiated.");
-        if (!message.member.permissions.has('MOVE_MEMBERS')) {
-            return message.reply("you are not allowed to use this command.\nYou need to be allowed to move members to execute this.");
-        }
 		if (!args.length && message.member.voice.channel) {
                 const voiceChannel = message.member.voice.channel;
 
@@ -19,9 +16,12 @@ module.exports = {
             
         }
         else if (!args.length) {
-            message.reply('you need to be connected to a voice channel to use this command.');
+            return message.reply('you need to be connected to a voice channel to use this command.');
         }
         else if (message.mentions.users.size>=1 && message.mentions.members.first().voice.channel) {
+            if (!message.member.permissions.has('MOVE_MEMBERS')) {
+                return message.reply("you are not allowed to use this command.\nYou need to be allowed to move members to execute this.");
+            }
             const voiceChannel = message.mentions.members.first().voice.channel;
             voiceChannel.join().then(connection => {
             const stream = 'https://media.vocaroo.com/mp3/jWCRHXWEsTT';
