a6ed23962867ddb88f18dde0910e22cdfb8c7f3b
suptower <70473844+suptower@users.noreply.github.com>
2020-09-05 15:58:35 +0200

    cooldown apache

--- commands/apache.js
+++ commands/apache.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 module.exports = {
 	name: 'apache',
     description: 'APACHE BLEIBT GLEICH',
-    cooldown: 10,
+    cooldown: 30,
 	execute(message, args) {
         let stream = "";
         const auRole = message.guild.roles.cache.find(role => role.name === "AUSOUP"); 
