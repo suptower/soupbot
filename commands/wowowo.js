35397a848a8dba694580776cf3af8952fcca99be
suptower <70473844+suptower@users.noreply.github.com>
2020-09-12 14:30:10 +0200

    vn.js + wowowo.js

---
+++ commands/wowowo.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+module.exports = {
+	name: 'wowowo',
+    description: 'WOWOWO',
+    cooldown: 30,
+	execute(message, args) {
+        let stream = "";
+        const auRole = message.guild.roles.cache.find(role => role.name === "AUSOUP"); 
+        console.log("wowowo command has been initiated.");
+        var audios = ['https://media1.vocaroo.com/mp3/1lbPjqC7uq1F','https://media1.vocaroo.com/mp3/140w4clWsA8T','https://media1.vocaroo.com/mp3/12KkEEcdcSDP','https://media1.vocaroo.com/mp3/1iAHw82kTumP'];
+        let value = Math.round(Math.random()*(audios.length-1));
+        stream = audios[value];
+    if (!args.length && message.member.voice.channel) {
+        const voiceChannel = message.member.voice.channel;
+
+        voiceChannel.join().then(connection => {
+            const dispatcher = connection.play(stream);
+            dispatcher.on('finish',() => voiceChannel.leave());
+    });
+        
+    }
+    else if (!args.length) {
+        return message.reply('you need to be connected to a voice channel to use this command.');
+    }
+    else if (message.mentions.users.size>=1 && message.mentions.members.first().voice.channel) {
+        if (!auRole) {
+            return message.reply("this server has not configured 'AUROLE'.\nPlease create this role to configure permissions for alternative usage of commands.");
+        }
+        else {
+            if (!message.member.roles.cache.has(auRole.id) && !message.member.permissions.has('ADMINISTRATOR')) {
+                return message.reply("you are not allowed to use this command.\nYou need to have 'AUSOUP' role assigned to execute.");
+            }
+            else {
+                const voiceChannel = message.mentions.members.first().voice.channel;
+                voiceChannel.join().then(connection => {
+                const dispatcher = connection.play(stream);
+                dispatcher.on('finish',() => voiceChannel.leave());
+                })
+            }
+            
+        }
+    }
+    else {
+        message.reply(`this user is currently not connected to any voice channel.`);
+        }
+    },
+};
