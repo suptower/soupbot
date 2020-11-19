f91d0f6cbd268705eb77f3faf31c42095290db66
suptower <70473844+suptower@users.noreply.github.com>
2020-11-19 12:24:56 +0100

    apored info update
    lets goo

--- commands/apored.js
+++ commands/apored.js
@@ -43,6 +43,44 @@ module.exports = {
             
         }
     }
+    else if (!(isNaN(parseInt(args[0])))) {
+        if (parseInt(args[0])>=0 && parseInt(args[0]) < audios.length) {
+            stream = audios(parseInt(args[0]));
+            if (!args[1] && message.member.voice.channel) {
+                const voiceChannel = message.member.voice.channel;
+
+                voiceChannel.join().then(connection => {
+                    const dispatcher = connection.play(stream);
+                    dispatcher.on('finish',() => voiceChannel.leave());
+                })
+            }
+            else if (message.mentions.users.size>=1 && message.mentions.members.first().voice.channel) {
+                if (!auRole) {
+                    return message.reply("this server has not configured 'AUROLE'.\nPlease create this role to configure permissions for alternative usage of commands.");
+                }
+                else {
+                    if (!message.member.roles.cache.has(auRole.id) && !message.member.permissions.has('ADMINISTRATOR')) {
+                        return message.reply("you are not allowed to use this command.\nYou need to have 'AUSOUP' role assigned to execute.");
+                    }
+                    else {
+                        const voiceChannel = message.mentions.members.first().voice.channel;
+                        voiceChannel.join().then(connection => {
+                        const dispatcher = connection.play(stream);
+                        dispatcher.on('finish',() => voiceChannel.leave());
+                        })
+                    }
+                    
+                }
+            }
+            else {
+                message.reply('No connection available.');
+            }
+        }
+    }
+    else if (args[0] === "info") {
+        message.channel.send("0 - Babawagen\n1 - Billo\n2 - Bruder muss los\n3 - Emblem\n4 - Everyday Saturday 1\n5 - Everyday Saturday 2\n6 - Lambo Gallardo\n7 - Numero Uno\n8 - Photoshop\n"
+        + "9 - Range Rover Mansory\n10 - Submariner\n11 - Yalla Habibi");
+    }
     else {
         message.reply(`this user is currently not connected to any voice channel.`);
         }
