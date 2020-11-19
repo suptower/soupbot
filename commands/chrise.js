6ed1154f9c57837d1fd606e54df7749503bca62f
suptower <70473844+suptower@users.noreply.github.com>
2020-11-19 19:18:55 +0100

    chrise schrei 3 und 4

--- commands/chrise.js
+++ commands/chrise.js
@@ -10,7 +10,7 @@ module.exports = {
         var audios = ['./chrise/chrisesus.mp3','./chrise/dimitriruntervonmir.mp3','./chrise/eiercreme.mp3',
             './chrise/ichweissnichtwas.mp3','./chrise/tuerkischeasy.mp3','./chrise/wtfmyfriend.mp3','./assets/mo5i.mp3','./chrise/mosesdukannstnix.mp3',
         './chrise/goenndoch.mp3','./chrise/schrei1.mp3','./chrise/eywas.mp3','./chrise/offense.mp3','./chrise/eztilt.mp3','./chrise/fullint.mp3','./chrise/andigehdochweg.mp3',
-        './chrise/keineahnung.mp3','./chrise/schrei2.mp3'];
+        './chrise/keineahnung.mp3','./chrise/schrei2.mp3','./chrise/schrei3.mp3','./chrise/schrei4.mp3'];
         let value = Math.round(Math.random()*(audios.length-1));
         stream = audios[value];
     if (!args.length && message.member.voice.channel) {
@@ -82,7 +82,7 @@ module.exports = {
     else if (args[0] === "info") {
         message.channel.send("0 - Chrise sus\n1 - Dimitri, runter von mir\n2 - Eiercreme\n3 - Ich weiss nicht\n4 - Turkish for Anfänger\n5 - wtf my friend?\n6 - YAAAAA MOSIIIII"
         +"\n7 - MOSES DU KANNST NIX\n8 - GÖNN DOCH\n9 - Schrei 1\n10 - EY WAS IST DES DENN\n11 - Schlechtester Offense überhaupt\n12 - EZ TILT\n13 - Full int\n"
-        +"14 - Ganz Casual Tilt gegen Andi\n15 - Keine Ahnung\n16 - Schrei 2");
+        +"14 - Ganz Casual Tilt gegen Andi\n15 - Keine Ahnung\n16 - Schrei 2\n17 - Schrei 3\n18 - Schrei 4");
     }
     else {
         message.reply(`this user is currently not connected to any voice channel.`);
