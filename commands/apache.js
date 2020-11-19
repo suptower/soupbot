f990fbe81690c3803a5cf7db9889c78cd6492e1e
suptower <70473844+suptower@users.noreply.github.com>
2020-11-19 12:01:36 +0100

    apache info
    test update

--- commands/apache.js
+++ commands/apache.js
@@ -42,6 +42,43 @@ module.exports = {
             
         }
     }
+    else if (!(isNaN(parseInt(args[0])))) {
+        if (parseInt(args[0])>=0 && parseInt(args[0]) < audios.length) {
+            stream = audios(parseInt(args[0]));
+            if (!args[1] && message.member.voice.channel) {
+                const voiceChannel = message.member.voice.channel;
+
+                voiceChannel.join().then(connection => {
+                    const dispatcher = connection.play(stream);
+                    dispatcher.on('finish',() => voiceChannel.leave());
+                })
+            }
+            else if (message.mentions.users.size>=1 && message.mentions.members.first().voice.channel) {
+                if (!auRole) {
+                    return message.reply("this server has not configured 'AUROLE'.\nPlease create this role to configure permissions for alternative usage of commands.");
+                }
+                else {
+                    if (!message.member.roles.cache.has(auRole.id) && !message.member.permissions.has('ADMINISTRATOR')) {
+                        return message.reply("you are not allowed to use this command.\nYou need to have 'AUSOUP' role assigned to execute.");
+                    }
+                    else {
+                        const voiceChannel = message.mentions.members.first().voice.channel;
+                        voiceChannel.join().then(connection => {
+                        const dispatcher = connection.play(stream);
+                        dispatcher.on('finish',() => voiceChannel.leave());
+                        })
+                    }
+                    
+                }
+            }
+            else {
+                message.reply('No connection available.');
+            }
+        }
+    }
+    else if (args[0] === "info") {
+        message.channel.send("0 - \n1 - \n2 - \n3 - \n4 -");
+    }
     else {
         message.reply(`this user is currently not connected to any voice channel.`);
         }
