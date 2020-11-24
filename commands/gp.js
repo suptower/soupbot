2fde48fba83852e16214c30485262bd98650f458
suptower <70473844+suptower@users.noreply.github.com>
2020-11-24 12:34:31 +0100

    gangsta

---
+++ commands/gp.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'gp',
+    cooldown: 15,
+	description: 'Use the gp feature.',
+	execute(message, args) {
+        console.log("gp command has been initiated.");
+        const auRole = message.guild.roles.cache.find(role => role.name === "AUSOUP");
+        if (!message.member.permissions.has('MOVE_MEMBER')) {
+            return message.reply("you are not allowed to use this command.\nYou need to be allowed to move members to execute this.");
+        }
+		if (!args.length && message.member.voice.channel) {
+                const voiceChannel = message.member.voice.channel;
+
+                voiceChannel.join().then(connection => {
+                    const stream = './assets/gp.mp3';
+                    const dispatcher = connection.play(stream);
+                    dispatcher.on('finish',() => voiceChannel.leave());
+            });
+            
+        }
+        else if (!args.length) {
+            message.reply('you need to be connected to a voice channel to use this command.');
+        }
+        else if (message.mentions.users.size>=1 && message.mentions.members.first().voice.channel) {
+            if (!auRole) {
+                return message.reply("this server has not configured 'AUSOUP'.\nPlease create this role to configure permissions for alternative usage of commands.");
+            }
+            else {
+                if (!message.member.roles.cache.has(auRole.id) && !message.member.permissions.has('ADMINISTRATOR')) {
+                    return message.reply("you are not allowed to use this command.\nYou need to have 'AUSOUP' role assigned to execute.");
+                }
+                const voiceChannel = message.mentions.members.first().voice.channel;
+                voiceChannel.join().then(connection => {
+                const stream = './assets/gp.mp3';
+                const dispatcher = connection.play(stream);
+                dispatcher.on('finish',() => voiceChannel.leave());
+                })
+            }
+                             
+            
+    }
+    else {
+        message.reply(`this user is currently not connected to any voice channel.`);
+    }
+	},
+};
