5ed949b1d3006c644f6a259a136f02e99c89f729
suptower <70473844+suptower@users.noreply.github.com>
2020-12-03 11:51:17 +0100

    wildes_update_haha
    andi.js, arda.js, exm.js update

--- commands/andi.js
+++ commands/andi.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
 module.exports = {
-	name: 'arda',
-    description: 'ze tilt itself',
+	name: 'andi',
+	description: 'oh no',
 	execute(message, args) {
         let stream = "";
         const auRole = message.guild.roles.cache.find(role => role.name === "AUSOUP"); 
-        console.log("arda command has been initiated.");
-        var audios = ["./arda/cheats.mp3","./arda/confidentthrow.mp3","./arda/desgibtsnicht.mp3","./arda/duhuanson.mp3","./arda/dunuttensohn.mp3","./arda/packsnicht.mp3","./arda/warnieweg.mp3",
-                    "./arda/schwanz.mp3","./arda/amerika.mp3","./arda/junge.mp3","./arda/nurweilichclippe.mp3","./arda/wohin.mp3"];
+        console.log("andi command has been initiated.");
+        var audios = ['./andi/100kmh.mp3','./andi/ambestengespielt.mp3','./andi/ardagoennen.mp3','./andi/aufstor.mp3','./andi/ballflach.mp3','./andi/chrise1.mp3',
+        './andi/chrise2.mp3','./andi/chrise3.mp3','./andi/clipoder.mp3','./andi/kante.mp3','./andi/kola.mp3','./andi/mosinab.mp3','./andi/ohnein.mp3','./andi/scheisse.mp3',
+        './andi/tilt.mp3','./andi/vergraben.mp3'];
         let value = Math.round(Math.random()*(audios.length-1));
         stream = audios[value];
     if (!args.length && message.member.voice.channel) {
@@ -77,8 +78,9 @@ module.exports = {
         }
     }
     else if (args[0] === "info") {
-        message.channel.send("0 - JAWOLL CHEATS\n1 - Condifent Throw\n2 - DES GIBTS NICHT\n3 - DU HUANSOHN\n4 - DU NUTTENSOHN\n5 - ICH PACKS NICHT\n6 - ICH WAR NIE WEG\n7 - Schwanz"
-        + "\n8 - AMERIKA SEINE MUTTER\n9 - JUNGE\n10 - NUR WEIL ICH CLIPPE\n11 - Ganz Casual Tilt gegen Chrise und Andi");
+        message.channel.send("0 - Unterwegs mit 100 km/h\n1 - ICH HAB AM BESTEN GESPIELT\n2 - Einfach Arda gönnen\n3 - \n4 - DER GEHT AUFS TOR\n5 - BALL FLACH HALTEN\n6 - Ganz Casual Tilt gegen Chrise 1"
+        +"\n7 - Ganz Casual Tilt gegen Chrise 2\n8 - Ganz Casual Tilt gegen Chrise 3\n9 - Du clippst schon oder?\n10 - Kante geben\n11 - Wie viel Kola hat Zucker?\n12 - Ganz Casual Tilt gegen Mosi\n13 - OH NEIN\n"
+        +"14 - Ganz Casual Tilt\n15 - Ganz Casual Tilt gegen Arda");
     }
     else {
         message.reply(`this user is currently not connected to any voice channel.`);
