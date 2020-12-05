be5f41472f07d693ba97d6efc83cbd9040979a0a
suptower <70473844+suptower@users.noreply.github.com>
2020-12-05 21:35:24 +0100

    1k5.js

---
+++ commands/1k5.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+module.exports = {
+	name: '1k5',
+	description: '1 KOMMA 5 MILLIONEN ABONENNTEN',
+	execute(message, args) {
+        console.log("1k5 command has been initiated.");
+        const auRole = message.guild.roles.cache.find(role => role.name === "AUSOUP");
+        if (!args.length && message.member.voice.channel) {
+            const voiceChannel = message.member.voice.channel;
+            voiceChannel.join().then(connection => {
+                const stream = './assets/1k5.mp3';
+                const dispatcher = connection.play(stream);
+                dispatcher.on('finish', () => voiceChannel.leave());
+            })
+        }
+        else if (!args.length) {
+            return message.reply("you need to be connected to a voice channel.");
+        }
+        else if (message.mentions.users.size>=1 && message.mentions.members.first().voice.channel) {
+            if (!auRole) {
+                return message.reply("this server has not configured 'AUSOUP'.\nPlease create this role to configure permissions for alternative usage of commands.");
+            }
+            else {
+                if (!message.member.roles.cache.has(auRole.id) && !message.member.permissions.has('ADMINISTRATOR')) {
+                    return message.reply("you are not allowed to use this command.\nYou need to have 'AUSOUP' role assigned to execute.");
+                }
+                else {
+                    const voiceChannel = message.mentions.members.first().voice.channel;
+                    voiceChannel.join().then(connection => {
+                        const stream = './assets/1k5.mp3';
+                        const dispatcher = connection.play(stream);
+                        dispatcher.on('finish', () => voiceChannel.leave());
+                    })
+                }
+                
+            }
+            
+        }
+        else {
+            return message.reply("this user is not connected to a voice channel.");
+        }
+	},
+};
