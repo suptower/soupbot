1f29ebe384846f9907c42a3a761d3760964954a5
suptower <70473844+suptower@users.noreply.github.com>
2020-12-31 14:13:47 +0100

    the big meme update
    why do you cum?

---
+++ commands/keanu.js
@@ -0,0 +1,85 @@
+const Discord = require('discord.js');
+module.exports = {
+	name: 'keanu',
+	description: 'DICK',
+	execute(message, args) {
+        let stream = "";
+        const auRole = message.guild.roles.cache.find(role => role.name === "AUSOUP"); 
+        console.log("andi command has been initiated.");
+        var audios = ['./keanu/dick.mp3','./keanu/fuckit.mp3','./keanu/gettingcock.mp3','./keanu/ucanbecum.mp3','./keanu/wellcum.mp3','./keanu/yduucum'];
+        let value = Math.round(Math.random()*(audios.length-1));
+        stream = audios[value];
+    if (!args.length && message.member.voice.channel) {
+        const voiceChannel = message.member.voice.channel;
+
+        voiceChannel.join().then(connection => {
+            const dispatcher = connection.play(stream);
+            dispatcher.on('finish',() => voiceChannel.leave());
+    });
+    }
+    else if (!args.length) {
+        return message.reply('you need to be connected to a voice channel to use this command.');
+    }
+    else if (!(isNaN(parseInt(args[0])))) {
+        if (parseInt(args[0])>=0 && parseInt(args[0]) < audios.length) {
+            stream = audios[parseInt(args[0])];
+            if (!args[1] && message.member.voice.channel) {
+                const voiceChannel = message.member.voice.channel;
+
+                voiceChannel.join().then(connection => {
+                    const dispatcher = connection.play(stream);
+                    dispatcher.on('finish',() => voiceChannel.leave());
+                })
+            }
+            else if (message.mentions.users.size>=1 && message.mentions.members.first().voice.channel) {
+                if (!auRole) {
+                    return message.reply("this server has not configured 'AUSOUP'.\nPlease create this role to configure permissions for alternative usage of commands.");
+                }
+                else {
+                    if (!message.member.roles.cache.has(auRole.id) && !message.member.permissions.has('ADMINISTRATOR')) {
+                        return message.reply("you are not allowed to use this command.\nYou need to have 'AUSOUP' role assigned to execute.");
+                    }
+                    else {
+                        const voiceChannel = message.mentions.members.first().voice.channel;
+                        voiceChannel.join().then(connection => {
+                        const dispatcher = connection.play(stream);
+                        dispatcher.on('finish',() => voiceChannel.leave());
+                        })
+                    }
+                    
+                }
+            }
+            else {
+                message.reply('No connection available.');
+            }
+        }
+        else {
+            return message.reply("your given number is not in accepted range.");
+        }
+    }
+    else if (message.mentions.users.size>=1 && message.mentions.members.first().voice.channel) {
+        if (!auRole) {
+            return message.reply("this server has not configured 'AUSOUP'.\nPlease create this role to configure permissions for alternative usage of commands.");
+        }
+        else {
+            if (!message.member.roles.cache.has(auRole.id) && !message.member.permissions.has('ADMINISTRATOR')) {
+                return message.reply("you are not allowed to use this command.\nYou need to have 'AUSOUP' role assigned to execute.");
+            }
+            else {
+                const voiceChannel = message.mentions.members.first().voice.channel;
+                voiceChannel.join().then(connection => {
+                const dispatcher = connection.play(stream);
+                dispatcher.on('finish',() => voiceChannel.leave());
+                })
+            }
+            
+        }
+    }
+    else if (args[0] === "info") {
+        message.channel.send("```0 - DICK\n1 - I'LL FUCK IT\n2 - GETTING COCK\n3 - YOU CAN BE CUM\n4 - WELL CUM\n5 - WHY DO YOU CUM?```");
+    }
+    else {
+        message.reply(`this user is currently not connected to any voice channel.`);
+        }
+	},
+};
---
+++ commands/wot.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'derprofi',
+    cooldown: 10,
+	description: 'I THOUGHT COLD WATER WAS SUPPOSED TO BOIL FASTER THAN HOT WATER',
+	execute(message, args) {
+        console.log("wot command has been initiated.");
+        const auRole = message.guild.roles.cache.find(role => role.name === "AUSOUP");
+		if (!args.length && message.member.voice.channel) {
+                const voiceChannel = message.member.voice.channel;
+
+                voiceChannel.join().then(connection => {
+                    const stream = './assets/wot.mp3';
+                    const dispatcher = connection.play(stream);
+                    dispatcher.on('finish',() => voiceChannel.leave());
+            });
+            
+        }
+        else if (!args.length) {
+            return message.reply('you need to be connected to a voice channel to use this command.');
+        }
+        else if (message.mentions.users.size>=1 && message.mentions.members.first().voice.channel) {
+            if (!auRole) {
+                return message.reply("this server has not configured 'AUSOUP'.\nPlease create this role to configure permissions for alternative usage of commands.");
+            }
+            else {
+                if (!message.member.roles.cache.has(auRole.id) && !message.member.permissions.has('ADMINISTRATOR')) {
+                    return message.reply("you are not allowed to use this command.\nYou need to have 'AUSOUP' role assigned to execute.");
+                }
+                else {
+                    const voiceChannel = message.mentions.members.first().voice.channel;
+                    voiceChannel.join().then(connection => {
+                    const stream = './assets/wot.mp3';
+                    const dispatcher = connection.play(stream);
+                    dispatcher.on('finish',() => voiceChannel.leave());
+                    })
+                }
+
+            }
+            
+                             
+            
+    }
+    else {
+        message.reply(`this user is currently not connected to any voice channel.`);
+    }
+	},
+};
